aa8defff5305fce749c60857800b1f6f3d0d48fc
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-03-02 16:36:58 +0330

    Create addpost_screen.dart

---
+++ instagram_ui/lib/screens/addpost_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text('add new Post'),
+          ],
+        ),
+      ),
+    );
+  }
+}
