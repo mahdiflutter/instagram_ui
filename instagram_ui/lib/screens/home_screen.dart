62b0585e020358ce9c18fa02a0db9a09b567d8a7
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-03-02 16:37:57 +0330

    Update home_screen.dart

--- instagram_ui/lib/screens/home_screen.dart
+++ instagram_ui/lib/screens/home_screen.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:instagram_ui/bloc/home/home_bloc.dart';
+import 'package:instagram_ui/bloc/home/home_event.dart';
+import 'package:instagram_ui/bloc/home/home_state.dart';
+import 'package:instagram_ui/model/post_model.dart';
+
+import 'package:instagram_ui/model/user_model.dart';
+import 'package:instagram_ui/widgets/cachedimage_widget.dart';
 import 'package:instagram_ui/widgets/post_widget.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,86 +19,63 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    BlocProvider.of<HomeBloc>(context).add(
+      HomeGetDataEvent(),
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            getHeader(),
-            getStories(),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) => Post(),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  SliverPadding getStories() {
-    return SliverPadding(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      sliver: SliverToBoxAdapter(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                color: const Color(0XFF000000).withOpacity(0.2),
-                width: 1,
-              ),
-              top: BorderSide(
-                color: const Color(0XFF000000).withOpacity(0.2),
-                width: 1,
-              ),
-            ),
-          ),
-          child: SizedBox(
-            height: 98,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.only(
-                  right: 20,
-                  top: 8,
-                  bottom: 8,
+      body: SafeArea(child: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          return CustomScrollView(
+            slivers: [
+              getHeader(),
+              if (state is HomeInitState) ...[
+                SliverToBoxAdapter(
+                  child: Row(),
+                )
+              ],
+              if (state is HomeLoadingState) ...[
+                const SliverToBoxAdapter(
+                  child: SpinKitRing(
+                    color: Colors.black,
+                    size: 30.0,
+                    lineWidth: 3,
+                  ),
+                )
+              ],
+              if (state is HomeSuccessResponseState) ...[
+                state.storiesResponse.fold(
+                  (storiesError) {
+                    return SliverToBoxAdapter(
+                      child: Text(storiesError),
+                    );
+                  },
+                  (storiesResponse) {
+                    return StoriesList(list: storiesResponse);
+                  },
                 ),
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(3),
-                      decoration: const ShapeDecoration(
-                        shape: CircleBorder(),
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomLeft,
-                          end: Alignment.topRight,
-                          colors: [
-                            Color(0xffFBAA47),
-                            Color(0xffD91A46),
-                            Color(0xffA60F93),
-                          ],
-                        ),
-                      ),
-                      child: Image.asset('assets/images/profile.png'),
-                    ),
-                    const SizedBox(
-                      height: 2,
-                    ),
-                    const Text(
-                      'mahdi_flutter',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
+                state.postsResponse.fold(
+                  (postsError) {
+                    return SliverToBoxAdapter(
+                      child: Text(postsError),
+                    );
+                  },
+                  (postsResponse) {
+                    return PostList(list: postsResponse);
+                  },
                 ),
-              ),
-            ),
-          ),
-        ),
-      ),
+              ],
+              
+            ],
+          );
+        },
+      )),
     );
   }
 
@@ -115,6 +101,112 @@ class _HomeScreenState extends State<HomeScreen> {
               'assets/icons/direct.png',
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class PostList extends StatelessWidget {
+  const PostList({super.key, required this.list});
+  final List<PostModel>? list;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        childCount: list!.length,
+        (context, index) {
+
+          return Post(post:list![index],);
+        },
+      ),
+    );
+  }
+}
+
+class StoriesList extends StatefulWidget {
+  const StoriesList({super.key, required this.list});
+  final List<UserModel>? list;
+
+  @override
+  State<StoriesList> createState() => _StoriesListState();
+}
+
+class _StoriesListState extends State<StoriesList> {
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      sliver: SliverToBoxAdapter(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: const Color(0XFF000000).withOpacity(0.2),
+                width: 1,
+              ),
+              top: BorderSide(
+                color: const Color(0XFF000000).withOpacity(0.2),
+                width: 1,
+              ),
+            ),
+          ),
+          child: SizedBox(
+            height: 115,
+            child: ListView.builder(
+              itemCount: widget.list!.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.only(
+                  right: 10,
+                  top: 8,
+                  bottom: 8,
+                ),
+                child: SizedBox(
+                  width: 65,
+                  height: 65,
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 65,
+                        height: 65,
+                        padding: const EdgeInsets.all(2),
+                        decoration: const ShapeDecoration(
+                          shape: CircleBorder(),
+                          gradient: LinearGradient(
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight,
+                            colors: [
+                              Color(0xffFBAA47),
+                              Color(0xffD91A46),
+                              Color(0xffA60F93),
+                            ],
+                          ),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(90),
+                          child: CachedImage(
+                            imgUrl: widget.list![index].avatar,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        '${widget.list![index].username}',
+                        maxLines: 1,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
