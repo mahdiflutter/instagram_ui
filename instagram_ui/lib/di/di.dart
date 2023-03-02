88a019d0c0c4b55ee324c9672103929559647c32
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-03-02 16:37:11 +0330

    Create di.dart

---
+++ instagram_ui/lib/di/di.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:instagram_ui/data/datasource/posts_datasource.dart';
+import 'package:instagram_ui/data/datasource/users_datasource.dart';
+import 'package:instagram_ui/data/repository/posts_repository.dart';
+import 'package:instagram_ui/data/repository/users_repository.dart';
+
+var locator = GetIt.instance;
+Future<void> initioalGetIt() async {
+  locator.registerSingleton<Dio>(
+    Dio(
+      BaseOptions(
+          baseUrl: 'https://6400512f63e89b0913abfc6c.mockapi.io/api/v1'),
+    ),
+  );
+  //data sources
+  locator.registerFactory<IUsersDataSource>(
+    () => UsersDataSource(),
+  );
+  locator.registerFactory<IPostsDataSource>(
+    () => PostsDataSource(),
+  );
+
+  //repository
+  locator.registerFactory<IUsersRepository>(
+    () => UsersRepository(),
+  );
+  locator.registerFactory<IPostsRepository>(
+    () => PostRepository(),
+  );
+}
